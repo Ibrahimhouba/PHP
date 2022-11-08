de4fee55b87c668ac27e0acf4a06cec9d18c465c
Ibrahimhouba <111587954+Ibrahimhouba@users.noreply.github.com>
2022-11-08 01:34:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,13 +32,29 @@ $table = "employee";
 
 try {
 
- $db= new PDO("pgsql:host=192.168.40.132;dbname=$database", $user, $password);
-  echo $t ."|". 'first_name' . "   |  " .  'last_name'. " |  " .'city_name'. " | ". 'email';
+        $db= new PDO("pgsql:host=192.168.40.132;dbname=$database", $user, $password);
+
   foreach($db->query("SELECT * FROM $table") as $row) {
-	 echo "<br>---------------------------------------------------------------------------|<br>";
-         echo $t ."|".$row['first_name'] . "   |  " . $row['last_name']. " |  " .$row['city_name']. " | ". $row['email'];
+
+echo "<table style='background-color:#D3D3D3' width=30% border='1'>
+
+<tr>
+<th <td>first_name</th></td>
+<th>last_name</th>
+<th >city_name</th>
+<th width>email</th>
+</tr>";
+
+echo "<tr>";
+echo "<td >" . $row['first_name'] . "</td>";
+echo "<td >" . $row['last_name'] . "</td>";
+echo "<td >" . $row['city_name'] . "</td>";
+echo "<td >" . $row['email'] . "</td>";
+echo "</tr>";
+echo "<table>";
+
   }
-  
+
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
